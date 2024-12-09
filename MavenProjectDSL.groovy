d88ffda2e98f3f469bda0888bbe1186e4f076900
Srujan <vsrujan@hotmail.com>
2024-12-09 12:06:25 -0600

    updated SCM on MavenProjectDSL.groovy 3

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -1,7 +1,7 @@
 job('First-Maven-Project-DSL') {
-    Description("First Maven project job via DSL on ${new Date()}")
+    Description("First Maven project job via DSL on ${new Date()}, ths project is small maven project on github")
     scm {
-        git("git@github.com:srujanvk/Jenkins_Upgradev3.git",master)
+        git("git@github.com:srujanvk/Jenkins_Upgradev3.git",'master')
     }
     triggers{
         scm('* * * * *')
